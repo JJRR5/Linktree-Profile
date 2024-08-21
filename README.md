ccece9ee2ba53138ec9635b2870042eb1ea36999
JJRR5 <jjrr_5@hotmail.com>
2024-08-21 16:07:55 -0600

    [ADD] prettier

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 
 This project is a public profile page similar to Linktree, built with Astro. It demonstrates skills in design, code structure, scalability, and CSS handling without the use of preprocessors or Tailwind.
 
-
 ### Instructions to run the project
 
 1. Clone the repository
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,19 @@
 import { z, defineCollection } from "astro:content";
 
 const userCollection = defineCollection({
-    type: 'data',
-    schema: ({image}) => z.object({
-        name: z.string(),
-        description: z.string(),
-        avatar: image().refine((img) => img.width < 1200, {
-            message: 'Image width must be greater than 5000px'
-        }),
-        socialMedia: z.array(z.string()),
-    })
-})
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      description: z.string(),
+      avatar: image().refine((img) => img.width < 1200, {
+        message: "Image width must be greater than 5000px",
+      }),
+      socialMedia: z.array(z.string()),
+    }),
+});
 
 export const collections = {
-    // the key is the name of the collection
-    'user': userCollection,
-}
+  // the key is the name of the collection
+  user: userCollection,
+};
